c1b2e67421fae77e6013174c06d3961eb4be1f4f
Shubham Pramanick <shupramanick@deloitte.com>
2022-01-23 22:36:47 +0530

    sub repo

---
+++ client/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="checkBalance">Check Balance</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import getWeb3 from './utils/getWeb3.js';
+import FToken from './contracts/FrustratedToken.json';
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data(){
+    return {
+      accounts:[],
+      FContractInstance:"",
+      loaded:false,
+      isMinted:false,
+      imgsrc:'',
+    }
+  },
+  async created(){
+    try{
+    let web3= await getWeb3();
+      console.log(web3)
+
+      const accounts = await web3.eth.getAccounts();
+
+      const networkId = await web3.eth.net.getId();
+
+      const FContractInstance = new web3.eth.Contract(
+          FToken.abi,
+          FToken.networks[networkId] && FToken.networks[networkId].address,
+      );
+
+      this.$nextTick(()=>{
+        this.accounts=accounts;
+        this.FContractInstance=FContractInstance;
+      });
+    }
+    catch (error) {
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+
+  },
+  methods:{
+    async checkBalance(){
+      let a = await this.FContractInstance.methods.totalSupply().call({from:this.accounts[0]});
+      let b = await this.FContractInstance.methods.checkBalance().call({from:this.accounts[0]});
+      console.log("balance check",this.FContractInstance,a,b);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
